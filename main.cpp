f2f82e47ee4f04f0107c1788fd023918f1562f1f
yuanfu zheng <2369050467@qq.com>
2024-08-29 00:22:16 +0800

    pra2 add main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#define DEBUG
+#ifdef DEBUG
+
+#include <iostream>
+#include <string>
+
+void main()
+{
+	std::string line;
+	std::getline(std::cin, line);
+	std::cout << line << std::endl;
+}
+
+
+
+
+#endif // DEBUG
